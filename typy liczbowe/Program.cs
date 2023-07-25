2925483db7e2ffd1eef053a235f7f9bb18d3aa16
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 13:36:13 +0200

    typy liczbowe i .git

---
+++ typy liczbowe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace typy_liczbowe
+{
+    class Program
+    {
+        double wR;
+        int dzielna = 7;
+
+        wR = (float) dzielna / 2;
+
+        Console.WriteLine(wR);
+	}
+}
